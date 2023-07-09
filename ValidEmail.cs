3467d36625d4d2adb2418349e0f41f6d728dbc8e
thines01 <thines01@yahoo.com>
2023-07-08 20:52:30 -0500

    Add project files.

---
+++ ValidEmail.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ValidEmail
+{
+   public class CValidEmail
+   {
+      public static bool isValidEmail(string strEmailAddress)
+      {
+         string strRegex = @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+               @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+               @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+
+         return (new Regex(strRegex)).IsMatch(strEmailAddress);
+      }
+   }
+}
